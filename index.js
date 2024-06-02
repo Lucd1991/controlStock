f25bf1df9da4d9405b231bea5d779a526cb50243
lucd1991 <lucas.diraddo@gmail.com>
2024-06-02 19:29:42 -0300

    Agrego html con formulario de usuario y contraseña

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/login', (req, res) => {
   res.status(200).json({
     result: 'Control de stock ISP'
   });
 });
 
-app.listen(port, () => {
-  console.log('app is running at port:', port);
+app.listen(PORT, () => {
+  console.log(`App is running at port: ${PORT}`);
 });
